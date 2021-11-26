4c7b401d92f0768648d338ea04005dc54345f48a
andvargrad <andvargrad@gmail.com>
2021-11-26 14:04:38 +0300

    Новая версия - распознования номера по картинке в .\image - замена лишних символов - добабвление ссылки - согранение в столбик

--- test/main.py
+++ test/main.py
@@ -1,13 +1,17 @@
 import pytesseract
 from PIL import Image
 import os
-def number():
+def save_number():
     for scan_name_file in os.listdir("image"):
         full_link = (os.path.abspath(f"image\{scan_name_file}"))
         pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
         img = Image.open(full_link)
         text = pytesseract.image_to_string(img)
-        numb = (text.replace(" ", ""))
+        numb = (text.replace(" ", "").strip())
         numb2 = (numb.replace("-", ""))
-        print(numb2.strip())
-number()
+        numb3 = (numb2.replace("8", "+7", 1))
+        link_numb = (f"https://wa.me/{numb3}")
+        with open("log.txt", "a") as save_file:
+            save_file.write(link_numb + "\n")
+
+save_number()
