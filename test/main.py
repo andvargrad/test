bb2a5b44960c840d39eae6ebf11455b97c18b38c
andvargrad <andvargrad@gmail.com>
2021-11-26 14:45:08 +0300

    Новая версия - распознования номера по картинке в .\image - замена лишних символов - добабвление ссылки - согранение в столбик

--- test/main.py
+++ test/main.py
@@ -1,17 +1,20 @@
 import pytesseract
 from PIL import Image
 import os
+
+#ФУНКЦИЯ РАСПОЗНОВАНИЯ НОМЕРА И СОХРАНЕНИЕ В НУЖНОМ ФОРМАТЕ V1.0
 def save_number():
-    for scan_name_file in os.listdir("image"):
-        full_link = (os.path.abspath(f"image\{scan_name_file}"))
-        pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
-        img = Image.open(full_link)
-        text = pytesseract.image_to_string(img)
-        numb = (text.replace(" ", "").strip())
-        numb2 = (numb.replace("-", ""))
-        numb3 = (numb2.replace("8", "+7", 1))
-        link_numb = (f"https://wa.me/{numb3}")
-        with open("log.txt", "a") as save_file:
+    for scan_name_file in os.listdir("image"):                                                          #листает все файлы в папке ./image
+        full_link = (os.path.abspath(f"image\{scan_name_file}"))                                        #полный поть до файла
+        pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"       #
+        img = Image.open(full_link)                                                                     #открывает фаил .JPG or .PNG
+        text = pytesseract.image_to_string(img)                                                         #конвертирует текст в строку
+        numb = (text.replace(" ", "").strip())                                                          #убираем лишнии пробелы
+        numb2 = (numb.replace("-", ""))                                                                 #убираем лишнии символы
+        numb3 = (numb2.replace("8", "+7", 1))                                                           #меняем 8 на +7
+        link_numb = (f"https://wa.me/{numb3}")                                                          #подставляем ссылку
+        with open("log.txt", "a") as save_file:                                                         #сохраняем в файл log.txt
             save_file.write(link_numb + "\n")
 
-save_number()
+save_number()      #ЗАПУСК ФУНКЦИИ
+
